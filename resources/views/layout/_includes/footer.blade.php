ef215426effcda7d2952f0a2b3f4eda7dd3c2cd1
Plinio <plininhodf@gmail.com>
2022-04-26 18:12:31 -0300

    adicionado pasta index, adicionar, _form

--- resources/views/layout/_includes/footer.blade.php
+++ resources/views/layout/_includes/footer.blade.php
@@ -1,2 +1,13 @@
+<!--JavaScript at end of body for optimized loading-->
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        M.updateTextFields();
+    $(document).ready(function(){
+        $('.sidenav').sidenav();
+  });
+    });
+</script>
 </body>
 </html>
--- resources/views/layout/_includes/topo.blade.php
+++ resources/views/layout/_includes/topo.blade.php
@@ -1,9 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+    <head>
     <title>@yield('titulo')</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
+<header>
+    <nav>
+        <div class="nav-wrapper deep-orange">
+            <div class="container">
+          <a href="#!" class="brand-logo">Logo</a>
+          <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="/">Home</a></li>
+            <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+          </ul>
+
+
+            <ul class="sidenav" id="mobile">
+                <li><a href="/">Home</a></li>
+                <li><a href="{{route('admin.cursos')}}">Cursos</a></li>
+
+            </ul>
+        </div>
+        </div>
+    </nav>
+</header>
